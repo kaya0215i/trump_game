9324f61469aace6e75cbd647d59beb6beba02058
kaya0215i <241209@st.yoshida-g.ac.jp>
2025-06-20 12:05:02 +0900

    #2 入力された4つの値を確認し、判定を行う

--- TrumpGame/Program.cs
+++ TrumpGame/Program.cs
@@ -10,7 +10,8 @@ namespace TrumpGame {
 
             int[] num = new int[4];
 
-            for(int i=0;i<num.Length;i++) {
+            // 入力処理
+            for(int i = 0; i < num.Length; i++) {
                 while(true) {
 
                     Console.Write($"{i+1}番目の数字を入力 >");
@@ -31,12 +32,99 @@ namespace TrumpGame {
                 }
             }
 
-
-            foreach (var val in num) {
-                Console.WriteLine(val);
+            // 判定処理
+            // フォーカード
+            if ((num[0] == num[1]) && (num[0] == num[2]) && (num[0] == num[3])) {
+                Console.WriteLine("フォーカード!!");
+                Console.ReadLine();
+                return;
             }
 
+            // スリーカード
+            int nPoint = 0;
+
+            for (int i = 0; i < 4; i++) {
+                nPoint = 0;
+                for (int n = 0; n < 4; n++) {
+                    if (num[i] == num[n]) {
+                        nPoint++;
+                    }
+                }
+
+                if (nPoint == 3) {
+                    Console.WriteLine("スリーカード!!");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+
+            // ツーペア
+            nPoint = 0;
+            int[] usedNum = new int[2];
+
+            for (int i = 0; i < 4; i++) {
+                if (num[i] == usedNum[0] || num[i] == usedNum[1]) {
+                    continue;
+                }
+
+                for (int n = 0; n < 4; n++) {
+                    if (i == n) {
+                        continue;
+                    }
+                    if (num[i] == num[n]) {
+                        nPoint++;
+
+                        if (usedNum[0] == 0) {
+                            usedNum[0] = num[i];
+                        }
+                        else {
+                            usedNum[1] = num[i];
+                        }
+                        break;
+                    }
+                }
+
+                if (nPoint == 2) {
+                    Console.WriteLine("ツーペア!!");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+            // ワンペア
+            nPoint = 0;
+            int used = 0;
+
+            for (int i = 0; i < 4; i++) {
+                if (num[i] == used) {
+                    continue;
+                }
+
+                for (int n = 0; n < 4; n++) {
+                    if (i == n) {
+                        continue;
+                    }
+                    if (num[i] == num[n]) {
+                        nPoint++;
+
+                        if (used == 0) {
+                            used = num[i];
+                        }
+                        break;
+                    }
+                }
+
+                if (nPoint == 1) {
+                    Console.WriteLine("ワンペア!!");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+            Console.WriteLine("ノーペア");
             Console.ReadLine();
+            return;
         }
     }
 }
